b8df69604537d1a1e169454efe9fb469d00285dd
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-02-27 17:52:48 -0500

    Updates to cell

--- RVenture/RVenture/View Controllers/ListingViewController.swift
+++ RVenture/RVenture/View Controllers/ListingViewController.swift
@@ -12,7 +12,7 @@ class ListingViewController: UIViewController, UICollectionViewDataSource, UICol
 
     // MARK - Properties
     // For UI testing
-    let locationName = ["The Shire", "Olympis Mons", "Gotham City"]
+    let locationName = ["The Shire", "Olympus Mons", "Gotham City"]
     let locationImage = [UIImage(named: "resize-1"),UIImage(named: "resize-2"),UIImage(named: "resize-3")]
     let locationPrice = ["$10/night", "$25/night", "$30/night"]
     let locationDescription = ["Lots of grass", "Interstellar views", "Dark Knight glamping"]
@@ -41,11 +41,12 @@ class ListingViewController: UIViewController, UICollectionViewDataSource, UICol
         cell.contentView.layer.cornerRadius = 6.0
         cell.contentView.layer.borderWidth = 1.0
         cell.contentView.layer.borderColor = UIColor.clear.cgColor
-        cell.contentView.layer.masksToBounds = false
-        cell.layer.shadowColor = UIColor.gray.cgColor
+        cell.contentView.layer.masksToBounds = true
+        
+        cell.layer.shadowColor = UIColor.lightGray.cgColor
         cell.layer.shadowOffset = CGSize(width: 0, height: 1.0)
         cell.layer.shadowRadius = 4.0
-        cell.layer.shadowOpacity = 1.0
+        cell.layer.shadowOpacity = 0.50
         cell.layer.masksToBounds = false
         cell.layer.shadowPath = UIBezierPath(roundedRect: cell.bounds, cornerRadius: cell.contentView.layer.cornerRadius).cgPath
         
