66edde88f3bbf578c50e589ad38948f8902b7841
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-02-28 15:45:33 -0500

    Set up detail storyboard

---
+++ RVenture/RVenture/View Controllers/ListingDetailViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ListingDetailViewController.swift
+//  RVenture
+//
+//  Created by Joshua Rutkowski on 2/27/20.
+//  Copyright © 2020 Gerardo Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ListingDetailViewController: UIViewController {
+    
+    var listingController: ListingController?
+    
+    // MARK:- IBOutlets
+    @IBOutlet weak var locationImage: UIImageView!
+    @IBOutlet weak var locationName: UILabel!
+    @IBOutlet weak var locationDescription: UILabel!
+    @IBOutlet weak var locationPrice: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
