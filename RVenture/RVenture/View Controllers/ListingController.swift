eace6987a956e2d9127d1243e77342e011e08b2f
Gerardo <gahernandez02@gmail.com>
2020-03-02 20:35:58 -0800

    Added Search function

--- RVenture/RVenture/View Controllers/ListingController.swift
+++ RVenture/RVenture/View Controllers/ListingController.swift
@@ -34,7 +34,51 @@ class ListingController {
     let locationDescription = ["Lots of grass", "Interstellar views", "Dark Knight glamping"]
     
     
-    var listings: [ListingRepresentations] = []
+    var searchedListings: [ListingRepresentation] = []
+    
+    init() {
+        fetchListingsFromServer()
+    }
+    
+    func searchForListing(with searchTerm: String, completion: @escaping (Error?) -> Void) {
+           
+           var components = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+           
+        //TODO: QUERY PARAMETERS From backend
+////           let queryParameters = ["query": searchTerm,
+////                                  "api_key": apiKey]
+//
+//           components?.queryItems = queryParameters.map({URLQueryItem(name: $0.key, value: $0.value)})
+//
+           guard let requestURL = components?.url else {
+               completion(NSError())
+               return
+           }
+           
+           URLSession.shared.dataTask(with: requestURL) { (data, _, error) in
+               
+               if let error = error {
+                   NSLog("Error searching for movie with search term \(searchTerm): \(error)")
+                   completion(error)
+                   return
+               }
+               
+               guard let data = data else {
+                   NSLog("No data returned from data task")
+                   completion(NSError())
+                   return
+               }
+               
+               do {
+                   let listingRepresentations = try JSONDecoder().decode(ListingRepresentations.self, from: data).results
+                   self.searchedListings = listingRepresentations
+                   completion(nil)
+               } catch {
+                   NSLog("Error decoding JSON data: \(error)")
+                   completion(error)
+               }
+           }.resume()
+       }
     
     func fetchListingsFromServer(completion: @escaping CompletionHandler = { _ in }) {
         let requestURL = baseURL.appendingPathComponent("Listing").appendingPathComponent("json")
